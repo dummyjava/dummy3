f692657b20c159b8a0b8bc1aafd898da69f865d6
sap <sap@192.168.2.17>
2018-08-09 07:17:04 -0400

    tst

---
+++ dummy3/src/main/java/com/dummy/java/dummy3/Application.java
@@ -0,0 +1,15 @@
+package com.dummy.java.dummy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       SpringApplication.run(Application.class, args);
+       System.out.println("heelospringboot");
+	}
+
+}
